2fb6a53dedc213553d59ef9993a7639db51ac5ae
Samuel SIlva <samuelsilvawb@gmail.com>
2018-05-02 19:48:24 -0300

    feat : create cars module
    - created module

---
+++ src/services/mysql/cars.js
@@ -0,0 +1,92 @@
+const _table = 'cars';
+const carsModule = deps => (
+	({
+		all: () => {
+			const { connection, errorHandler } = deps;
+			return new Promise((resolve, reject) => {
+				connection.query(`SELECT * FROM ${_table};`, (error, results) => {
+					if (error) {
+						errorHandler(
+							error,
+							'Error to list cars',
+							reject,
+						);
+						return;
+					}
+
+					resolve({ cars: results });
+				});
+			});
+		},
+		save: (modelCar, yearCar, colorCar) => {
+			const { connection, errorHandler } = deps;
+
+			return new Promise((resolve, reject) => {
+				connection.query(`INSERT INTO ${_table}(modelCar, yearCar, colorCar) VALUES (?, ?, ?);`, [modelCar, yearCar, colorCar], (error, results) => {
+					if (error) {
+						errorHandler(
+							error,
+							'Error to save car',
+							reject,
+						);
+						return;
+					}
+
+					resolve({
+						car: {
+							modelCar, yearCar, colorCar, carID: results.insertId,
+						},
+					});
+				});
+			});
+		},
+		update: (id, modelCar, yearCar, colorCar) => {
+			const { connection, errorHandler } = deps;
+
+			return new Promise((resolve, reject) => {
+				connection.query(`UPDATE ${_table} SET modelCar = ?, yearCar = ?, colorCar = ? WHERE carID = ?;`, [
+					modelCar,
+					yearCar,
+					colorCar,
+					id,
+				], (error, results) => {
+					if (error || !results.affectedRows) {
+						errorHandler(
+							error,
+							'Error to update car',
+							reject,
+						);
+						return;
+					}
+
+					resolve({
+						car: {
+							modelCar, yearCar, colorCar, carID: id,
+						},
+						affectedRows: results.affectedRows,
+					});
+				});
+			});
+		},
+		del: (id) => {
+			const { connection, errorHandler } = deps;
+
+			return new Promise((resolve, reject) => {
+				connection.query(`DELETE FROM ${_table} WHERE carID = ?;`, [id], (error, results) => {
+					if (error || !results.affectedRows) {
+						errorHandler(
+							error,
+							'Error to delete car',
+							reject,
+						);
+						return;
+					}
+
+					resolve({ message: 'Car removed successfully!', affectedRows: results.affectedRows });
+				});
+			});
+		},
+	})
+);
+
+module.exports = carsModule;
